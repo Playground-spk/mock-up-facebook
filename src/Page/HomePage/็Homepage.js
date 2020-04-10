d48d059cd8b26ca9dc64085c01f7d241adee7492
spk-cpsamut <taresupakon@gmail.com>
2020-04-10 23:13:48 +0700

    doing

---
+++ src/Page/HomePage/็Homepage.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import { Col } from "antd";
+
+export default class Homepage extends Component {
+  render() {
+    return <Col className="Homepage"></Col>;
+  }
+}
